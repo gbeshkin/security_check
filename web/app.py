c434cc0a5a5e0d3b17caa4a23e489c510051d3db
grigorijs.beskins <grigorijs.beskins@kuehne-nagel.com>
2026-03-31 12:35:43 +0300

    Update app.py
    fix PORT for railway

--- web/app.py
+++ web/app.py
@@ -124,4 +124,4 @@ def health():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+        CMD sh -c "gunicorn --bind 0.0.0.0:$PORT wsgi:app"
